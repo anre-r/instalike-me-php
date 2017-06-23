3adbaa9bae3e908002ed3597d05aa41abcf01c24
Andrey Revkov <andre.revkov@gmail.com>
2017-06-23 20:46:18 +0300

    code formatting

--- instalike-me.php
+++ instalike-me.php
@@ -7,19 +7,19 @@
  */
 class InstaLikeMe
 {
-	private $apiKey;
-	private $url;
+    private $apiKey;
+    private $url;
 
-	/**
-	 * @author Andrey Revkov <andre.revkov@gmail.com>
-	 */
-	function __construct ($apiKey = 'YOUR-API-KEY', $version = 1)
-	{
-		$this->apiKey = $apiKey;
-		$this->url = "http://instalike.me/api/v{$version}/";
-	}
+    /**
+     * @author Andrey Revkov <andre.revkov@gmail.com>
+     */
+    function __construct ($apiKey = 'YOUR-API-KEY', $version = 1)
+    {
+        $this->apiKey = $apiKey;
+        $this->url = "http://instalike.me/api/v{$version}/";
+    }
 
-	/**
+    /**
      * Call API methods
      * 
      * @param string $method Method name
@@ -30,11 +30,11 @@ class InstaLikeMe
      * 
      * @author Andrey Revkov <andre.revkov@gmail.com>
      */
-	public function api ($method = 'ping', array $params = [], $format = 'json')
-	{
+    public function api ($method = 'ping', array $params = [], $format = 'json')
+    {
         $params['apiKey'] = $this->apiKey;
         $curl = curl_init();
-		curl_setopt_array(
+        curl_setopt_array(
             $curl,
             [
                 CURLOPT_URL => $this->url . $method . '.' . $format,
@@ -43,11 +43,11 @@ class InstaLikeMe
                 CURLOPT_POSTFIELDS => http_build_query($params)
             ]
         );
-		$response = curl_exec($curl);
-		curl_close($curl);
-        
+        $response = curl_exec($curl);
+        curl_close($curl);
+
         $data = json_decode($response, true);
 
-		return $data;
-	}
+        return $data;
+    }
 }
